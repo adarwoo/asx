ba591abee87401da71c944180dbcbaecc08cab17
Adarwoo <software@arreckx.com>
2025-01-20 08:47:43 +0100

    Simplifications throughout and stop isr

--- src/alert.c
+++ src/alert.c
@@ -9,13 +9,14 @@
  * @{
  */
 #include <avr/wdt.h>
+#include <util/delay.h>
 
 #include <assert.h>
 #include <ioport.h>
 
 // Need to include the configuration to determine the alert pin
-#include "conf_board.h"
-#include "alert.h"
+#include <board.h>
+#include <alert.h>
 
 #ifdef ALERT_RECORD
 #   ifndef ALERT_RECORD_OFFSET
@@ -28,7 +29,11 @@
  * For LED notification, we need to set the LED direction.
  * For EEPROM storage, we need to ready some variables
  */
-void alert_init( void )
+static void
+#ifndef _WIN32
+   __attribute__ ((section (".init5"), naked, used))
+#endif
+alert_init( void )
 {
 #ifdef ALERT_OUTPUT_PIN
    ioport_set_pin_dir( ALERT_OUTPUT_PIN, IOPORT_DIR_OUTPUT );
@@ -47,11 +52,16 @@ void alert_init( void )
  */
 void alert_record( bool doAbort, int line, const char *file )
 {
+   cli();
+
+   // Dump to the debug pin
+   #ifdef ALERT_OUTPUT_PIN
+   ioport_set_pin_level( ALERT_OUTPUT_PIN, true );
+   #endif
+
    // Output to stdout?
    #ifdef ALERT_TO_STDOUT
-   {
-      printf_P("ALERT: %s, line %d\n", file, line);
-   }
+   printf_P("ALERT: %s, line %d\n", file, line);
    #endif
 
    // Stop or not
@@ -62,43 +72,17 @@ void alert_record( bool doAbort, int line, const char *file )
          //  than reset
          wdt_disable();
       #endif
+
       // Lock forever. Breaking the execution will end up here
       //  (or an interrupt), and the trace will point to the
       //  culprit.
       // The watchdog will then reset.
       for (;;)
       {
-         // Dump to the debug pin
-         #ifdef ALERT_OUTPUT_PIN
-         {
-            // Transmit as X_X___X_X___XXX___X_X which can be read easily off any scope
-            uint_fast16_t l = line;
-            uint_fast8_t i, j;
-
-            // Transmit the 12-bits as 4 bits for easy read
-            for (i=0; i<12; ++i)
-            {
-               uint8_t tx = (l & 0x800) ? 0b011100 : 0b010100;
-
-               for (j=0; j<6; ++j)
-               {
-                  ioport_set_pin_level( ALERT_OUTPUT_PIN, (bool) (tx & 1) );
-                  tx >>= 1;
-               }
-
-               l <<= 1;
-            }
-
-            // Transmit 24*4 empty bits for the scope to resync easy
-            for (i=0; i<24*4; ++i)
-            {
-               ioport_set_pin_level( ALERT_OUTPUT_PIN, false );
-            }
-         }
-         #endif
-         continue;
       }
    }
+
+   sei();
 }
 
  /**@}*/
