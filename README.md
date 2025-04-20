da6fd9045afb7b84fadaf477330443a443ef8c4d
guillaume.arreckx <software@arreckx.com>
2025-04-20 16:05:19 +0200

    More comments

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 # asx
 <b>ASX</b> is a core library for building C and C++ application for the ATTiny2 family.
+It is based on the reactor pattern.
+The core library is less than 2KB of code.
+With ASX, use elements of the standard C++ library, like type_traits, chrono, array at
+next to no cost.
 
 ASX comes standard with:
 <ul>
    <li>C libraries remeniscent of the ASF library from Atmel</li>
-   <li>Modern TMP C++ libraries</li>
+   <li>Modern Template Meta Programming C++ libraries</li>
    <li>A C/C++ reactor engine, to execute asynchronous tasks</li>
    <li>A C/C++ timer service, integrated to the reactor</li>
    <li>The boost sml library for incredible state machines</li>
    <li>The standard C++ library ported to the AVR</li>
    <li>Memory allocation support, and memory fencing</li>
-   <li>A logger, with 0 cost logger for release builds</li>
+   <li>A memory trace library, with 0 cost logger for release builds</li>
    <li>A C++ modbus library, with ICD compiler written in Python</li>
    <li>A built-in build environment using Docker. Works on Linux and WSL2</li>
    <li>Makefile to build in docker, Windows native (including in AVR studio)</li>
 </ul>
 
 <h2>The reactor engine</h2>
-The reactor engine allows binding arbitry functions to have them called in the 'main' processing space (as opposed to inside interrupt context).
+The reactor engine allows binding arbitry functions to have them called in the 'main' processing space (as opposed to interrupt context).
 With the reactor, asynchronous events can be managed collaboratively without the need for synchronisation nor the risk of race condition.
 The reactor loop takes 17us to evaluate the next handler, and the notification (to promote a reactor), takes 1us.
 
@@ -26,12 +30,13 @@ The reactor is the ideal companion to your interrupts. Interrupt should simply e
 The actually handling of the data takes place in the main loop.
 
 The downside of the reactor is the lack of control over the latency.
-To ease the problem, the reactor has priorities, so reactor with higher importance gets served first.
+But no worries!
+1. The reactor has priorities, so a reactor handler with higher importance gets served first
+2. Handlers can yield time to other handlers, so long handling can be split to allow for a higher level handler to kick in
 
-The worse case latency is always going to be the longest possible handler.
-
-So the reactor works nicely when handling is quick.
-For functions that take up more time, the handling should be sliced.
+The worse case latency is always going to be the longest possible.
+The reactor can drive a debug pin to analyse which handlers may take too long.
+In the rare cases a handler is taking too long at the wrong time, it's processing should be sliced using the yield function.
 
 The reactor integrates a timer, so repeating reactors or delayed are possible.
 Timer can also be safely stopped without the fear of a race conditions.
@@ -39,34 +44,57 @@ Timer can also be safely stopped without the fear of a race conditions.
 <h2> Other words </h2>
 Finally, the ASX framework comes with all the goodies from the ASF project (which Microchip did not port to the Tiny2 familly which are very close to the XMega) as well as the full standard C++ library.
 
-It is designed to compile with a C++17 or C++20 compiler.
+It is designed to compile with a C++20 compiler, but comes with a Docker image which includes everything required.
+
+<h3> Working in Microchip studio </h3>
+To use with Atmel/Microchip studio, the toolchain will require upgrading from the aging 5.4! to a 14.0 or better.
+Check https://github.com/ZakKemble/avr-gcc-build/releases for the release binaries.
+Simply download the pre-build Windows image and place in
+<pre>C:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8</pre>
+Rename the folder 'avr8-gnu-toolchain' as 'avr8-gnu-toolchain-5.4'
+...and create a link 'avr8-gnu-toolchain' pointing to the toolchain.
 
 Example code:
 
 ```C++
-#include <chrono> // For the ""s
-#include <asx/reactor.hpp>
+#include <chrono>          // For the ""s
+#include <asx/ioport.hpp>  // Include before conf_board
+#include <asx/reactor.hpp> // Reactor API
 
-#include <conf_board.h>
+#include "conf_board.h"
 
 using namespace asx;
 using namespace asx::ioport;
 using namespace std::chrono;
 
-// Called by the reactor every second
-auto flash_led() -> void {
-   Pin(MY_LED).toggle();
-}
-
 // Initialise all and go
 auto main() -> int {
-   Pin(MY_LED).init(dir_t::out, value_t::high);
+   Pin(MY_LED).init(ioport::dir_t::out, ioport::value_t::high);
 
-   reactor::bind(flash_led).repeat(1s);
+   reactor::bind([]{Pin(MY_LED).toggle();}).repeat(1s);
    reactor::run();
 }
 ```
-  
-   
-    
-  
+
+The code is <2Kb when compiled in release mode, and the main block is 70bytes.
+
+Check the example directory which includes a make file
+
+<h1>How to build</h1>
+
+To try out the blink example, you need to have gitman installed.
+
+<h2>Building in Windows with WSL</h2>
+If you are using Visual Studio Code, start a WSL terminal:
+
+```sh
+# Install gitman
+pipx install gitman
+# Get ASX
+git clone https://github.com/adarwoo/asx
+gitman update
+# Make sure Docker-CE is running in Windows!
+cd example
+make
+```
+
--- include/asx/reactor.hpp
+++ include/asx/reactor.hpp
@@ -1,4 +1,26 @@
 #pragma once
+// MIT License
+//
+// Copyright (c) 2025 software@arreckx.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 /**
  * @file
  * C++ Reactor API declaration
@@ -79,6 +101,11 @@ namespace asx {
          struct function_traits : function_traits<decltype(&F::operator())> {};
 
          // Trampoline template for functions with 2 arguments
+         //
+         // On the AVR, a  void (*)(void*) pointer is passed in registers R24:R25.
+         // BUT, a function like void func(uint8_t, uint8_t) uses R24 and R22, not packed.
+         // This difference is critical on AVR — the trampoline must call the actual function
+         //  with two 8-bit arguments, not just reinterpret the function pointer.
          template <typename Func>
          struct Trampoline {
             static Func func;
@@ -94,6 +121,7 @@ namespace asx {
             }
          };
 
+         // Create instances of the trampoline for each function type
          template <typename Func>
          Func Trampoline<Func>::func;
       }
@@ -157,29 +185,57 @@ namespace asx {
             reactor_notify(handle, detail::pack(arg1, arg2));
          }
 
-         // Invoke (direct call of the handler) no arg
+         /**
+          * @brief Invoke the handler directly
+          * @details This is a direct call to the handler, bypassing the reactor.
+          * It then treats the handler like a regular function pointer.
+          * Use invoke when using Mask in your application.
+          */
          void invoke() {
             reactor_invoke(handle, nullptr);
          }
 
-         // Notify function for one argument
+         /**
+          * @brief Invoke the handler directly with a single argument
+          * @see invoke()
+          */
          template <typename T>
          inline void invoke(T arg) {
             reactor_invoke(handle, static_cast<uintptr_t>(arg));
          }
 
-         // Notify function for two arguments, packing them into a single 32-bit value
+         /**
+          * @brief Invoke the handler directly with a 2 arguments
+          * Both arguments are packed into a single 16-bit value, therefore each argument must be
+          * 8-bit long.
+          * @see invoke()
+          */
          template <typename T1, typename T2>
          inline void invoke(T1 arg1, T2 arg2) {
             reactor_invoke(handle, detail::pack(arg1, arg2));
          }
 
-         // Notify in the future
+         /**
+          * @brief Invoke the reactor handler after a delay from now
+          * @param after The time to wait before invoking the handler
+          * @return The underlying running timer instance. This is required to cancel the timer.
+          * @details A timer is armed with the given time and will invoke the handler
+          * after the given time.
+          * @note Do not use this function inside an interrupt context.
+          */
          inline timer::Instance delay(timer::duration after) {
             return timer_arm(handle, clock::to_timer_count(clock::now() + after), 0, nullptr);
          }
 
-         // Notify in the future
+         /**
+          * @brief Repeatingly invoke the reactor handler after a delay from now. This version
+          * all controlling the initial delay and the repeat time.
+          * @param after The time to wait before invoking the handler
+          * @param repeat The time to wait before invoking the handler again
+          * @return The underlying running timer instance. This is required to cancel the timer.
+          * @note Do not use this function inside an interrupt context.
+          * @see delay()
+          */
          inline timer::Instance repeat(timer::duration after, timer::duration repeat) {
             return timer_arm(
                handle,
@@ -191,6 +247,7 @@ namespace asx {
 
          /**
           * Start repeating invocations after 'repeat'
+          * @note Do not use this function inside an interrupt context.
           */
          inline timer::Instance repeat(timer::duration repeat) {
             return timer_arm(
@@ -201,13 +258,45 @@ namespace asx {
             );
          }
 
-         // Notify in the future
-         inline timer::Instance delay(timer::time_point at)
-         {
+         /**
+          * Invoke the handler at a specific time in the future
+          * @param at The timepoint when the handler is notified
+          * @note If the time is expired already, the handler is invoked in the next millisecond.
+          * @note Do not use this function inside an interrupt context.
+          */
+         inline timer::Instance delay(timer::time_point at) {
             return timer_arm(handle, clock::to_timer_count(at), 0, nullptr);
          }
 
-         // Notify in the future
+         /**
+          * Invoke the handler after a delay from now. This version allow passing a single
+          * argument to the handler.
+          * The argument must be castable to a uintptr_t (no more than 16bits).
+          * @param at The timepoint when the handler is notified
+          * @param arg The argument to pass to the handler
+          * @note If the time is expired already, the handler is invoked in the next millisecond.
+          * @note Do not use this function inside an interrupt context.
+          */
+         template <typename T>
+         inline timer::Instance delay(
+            timer::duration after, T arg) {
+            return timer_arm(
+               handle,
+               clock::to_timer_count(clock::now() + after),
+               0,
+               reinterpret_cast<void*>(static_cast<uintptr_t>(arg))
+            );
+         }
+
+         /**
+          * @brief Repeatingly invoke the handler at a given interval,
+          *         starting at a specific time in the future.
+          * @param at The timepoint when the handler is notified
+          * @param repeat The time to wait before invoking the handler again
+          * @return The underlying running timer instance. This is required to cancel the timer.
+          * @note If the time is expired already, the handler is invoked in the next millisecond.
+          * @note Do not use this function inside an interrupt context.
+          */
          inline timer::Instance repeat(timer::time_point at, timer::duration repeat)
          {
             return timer_arm(
@@ -218,20 +307,14 @@ namespace asx {
             );
          }
 
+         /**
+          * @brief Clear this reactor handler from the reactor
+          * Allows removing a handler from the reactor. If the handler is not in the reactor, the
+          * function has no effect.
+          * @note Do not use this function inside an interrupt context.
+          */
          inline void clear() {
             reactor_clear( 1UL << handle );
-         }
-
-         template <typename T>
-         inline timer::Instance delay(
-            timer::duration after, T arg)
-         {
-            return timer_arm(
-               handle,
-               clock::to_timer_count(clock::now() + after),
-               0,
-               reinterpret_cast<void*>(static_cast<uintptr_t>(arg))
-            );
          }
 
          template <typename T>
@@ -247,7 +330,29 @@ namespace asx {
          }
       };
 
-      ///< Shortcut for the C++ mask
+      /**
+       * @brief Reactor mask class
+       * @details This class is used to manage a set of reactor handles. (up to 32)
+       * It allows to create a mask of handles, append new handles to the mask,
+       * and check if the mask is empty.
+       * You can add reactor handles to the mask using the append() method.
+       * The pop method will return the highest priority handle and remove it from the mask.
+       * The is_empty() method checks if the mask is empty.
+       * Unlike a queue, the mask will not expand. Items added more than once are simple added.
+       * @note The mask is a bitwise representation of the handles.
+       * Each handle is represented by a single bit in the mask.
+       * @example
+       * reactor::Mask m;
+       * // h2 has a higher priority than h1 as low priority handlers are packed from the bottom.
+       * // The order of registration is important for the priority.
+       * reactor::Handle h1 = reactor::bind([](){...});
+       * reactor::Handle h2 = reactor::bind([](){...});
+       * m.append(h1);
+       * m.append(h2);
+       * while (not m.is_empty()) {
+       *    m.pop().invoke(); // Directly call the handler
+       * }
+       */
       class Mask {
          reactor_mask_t mask;
 
@@ -269,14 +374,14 @@ namespace asx {
             return Handle(reactor_mask_pop(&mask));
          }
 
-         /// @brief Append another reactor
+         /// @brief Append a reactor handle to the mask
          /// @param h A reactor handle
          void append(const Handle h) {
             mask |= reactor_mask_of(h);
          }
 
-         /// @brief Append another reactor
-         /// @param h A reactor handle
+         /// @brief Combine the handles from another mask
+         /// @param m A reactor mask
          void append(const Mask m) {
             mask |= m.mask;
          }
@@ -287,7 +392,22 @@ namespace asx {
          }
       };
 
-      // Register handler function
+      /**
+       * @brief Bind a function to the reactor
+       * @return A reactor handle object
+       * @details The function must be a callable object (function pointer, lambda, etc.).
+       * The function will be called when its handle is notified.
+       * The function must have a specific signature:
+       *  - No arguments
+       *  - One argument : must be castable to a uintptr_t
+       *  - Two arguments : Each argument size must be 8-bit long
+       * The function will be called with the arguments passed to the notify() method.
+       * @warning The function cannot not be a member function of a class.
+       * @warning The invoke/notify() methods do not check if the arguments passed
+       * match the prototype of the handler. A mismatch will result in undefined behavior.
+       * The function must be a callable object (function pointer, lambda, etc.). The function
+       * will be called when the reactor is notified.
+       */
       template <typename F>
       std::enable_if_t<detail::function_traits<std::decay_t<F>>::arity <= 1, Handle>
       bind(F&& func, reactor_priority_t p = prio::low) {
@@ -314,6 +434,32 @@ namespace asx {
          );
       }
 
+      template <typename C, typename Ret, typename... Args>
+      requires (
+         sizeof...(Args) == 2
+         && detail::EightBit<typename std::tuple_element<0, std::tuple<Args...>>::type>
+         && detail::EightBit<typename std::tuple_element<1, std::tuple<Args...>>::type>
+      )
+      Handle bind(Ret (C::*method)(Args...), C* instance, reactor_priority_t p = prio::low) {
+         // Create a lambda capturing the instance and method
+         auto thunk = [instance, method](Args... args) {
+            (instance->*method)(args...);
+         };
+
+         // Bind using existing 2-arg infrastructure (it will go through Trampoline)
+         return bind(thunk, p);
+      }
+
+      /**
+       * @brief Create a mask from a set of handles
+       * @param a The first handle
+       * @param args The remaining handles
+       * @return A mask containing all the handles
+       * @details This function creates a mask from a set of handles. The handles are combined
+       * using a bitwise OR operation. The resulting mask can be used to notify all the handles
+       * at once.
+       * @note The function uses a fold expression to combine the handles.
+       */
       template <typename... Args>
       static constexpr Mask mask_of(Handle a, Args... args) {
          reactor_mask_t m = reactor_mask_of(a);
@@ -328,27 +474,67 @@ namespace asx {
          reactor_clear(m);
       }
 
+      /**
+       * @brief Notify a reactor handler from an interrupt context. This function is fast.
+       * @param on_xx The reactor handler to notify
+       * @details This function is used to notify a reactor handler from an interrupt context.
+       * It is a no-op if the handler is not registered in the reactor.
+       * @note This function should only be used from an interrupt context.
+       * @note NULL arguments are passed to the handler
+       */
       static inline void notify_from_isr(Handle on_xx) {
          reactor_null_notify_from_isr(on_xx);
       }
 
-      // Yield - no arg
+      /**
+       * @brief yield execution to the reactor
+       * @details This function is used to yield execution to the reactor.
+       * It allows the reactor to process other handlers but automatically
+       *  returns to the caller when the reactor is done.
+       * This function does not take any arguments.
+       * To keep track of the state of the handler, a local static can be used.
+       */
       inline void yield() {
          reactor_yield(NULL);
       }
 
-      // Yield - single arg
+      /**
+       * @brief yield execution to the reactor with a single argument
+       * @details This function is used to yield execution to the reactor.
+       * It allows the reactor to process other handlers but automatically
+       *  returns to the caller when the reactor is done.
+       * This function takes a single argument which is passed to the handler.
+       * The argument must be castable to a uintptr_t (no more than 16bits).
+       * @param arg The argument to pass to the handler
+       * @note The argument can be used to determine the state of the handler.
+       */
       template <typename T>
       inline void yield(T arg) {
          reactor_yield(reinterpret_cast<void*>(static_cast<uintptr_t>(arg)));
       }
 
-      // Yield 2 args
+      /**
+       * @brief yield execution to the reactor with two arguments
+       * @see yield()
+       * @param arg1 The first argument to pass to the handler
+       * @param arg2 The second argument to pass to the handler
+       * @note The arguments must be castable to a uintptr_t (no more than 8bits).
+       * @note The arguments are packed into a single 16-bit value, therefore each argument must be
+       * 8-bit long.
+       */
       template <typename T1, typename T2>
       inline void yield(T1 arg1, T2 arg2) {
          reactor_yield(detail::pack(arg1, arg2));
       }
 
-      static inline void run() { reactor_run(); }
+      /**
+       * @brief Run the reactor
+       * @details This function is used to run the reactor.
+       * It should only be called once.
+       * This function never returns.
+       */
+      static inline void run() {
+         reactor_run();
+      }
    }
 } // End of namespace asx
